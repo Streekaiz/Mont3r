3447b842fd500969e12796cb62ae0df08b93a3f1
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2025-04-15 23:15:10 -0700

    file stuff probably doesnt work; likely syntax errors written on my phone lol

--- Modules/Library/builder.lua
+++ Modules/Library/builder.lua
@@ -41,26 +41,88 @@ function MONT3R_ENV:BuildFovCircle(Properties)
     Section:toggle({name = "use fov", flag = Flag .. "_fov_use"})
     Section:keybind({flag = Flag .. "_fov_use_keybind"})
     Section:toggle({name = "show fov", flag = Flag .. "_fov_show", callback = function(Value)
-             setrenderproperty(Config.Circle, "Visible", Value)
+        setrenderproperty(Config.Circle, "Visible", Value)
     end})
     Section:colorpicker({flag = Flag .. "_fov_color", color = Config.Color, alpha = 0, callback = function(Value)
-             setrenderproperty(Config.Circle, "Color", Value)
+        setrenderproperty(Config.Circle, "Color", Value)
     end})
     Section:toggle({name = "show fov outline", flag = Flag .. "_fov_outline_show", callback = function(Value)
-            		setrenderproperty(Config.OutlineCircle, "Visible", Value)
+        setrenderproperty(Config.OutlineCircle, "Visible", Value)
     end})
     Section:colorpicker({flag = Flag .. "_fov_outline_color", color = Config.OutlineColor, callback = function(Value)
-        						setrenderproperty(Config.OutlineCircle, "Color", Value)
+        setrenderproperty(Config.OutlineCircle, "Color", Value)
     end})
     Section:slider({name = "fov radius", max = 360, default = 180, suffix = " pixels", flag = Flag .. "_fov_radius", callback = function(Value)
-              setrenderproperty(Config.Circle, "Radius", Value)
-        						setrenderproperty(Config.OutlineCircle, "Radius", Value)
+        setrenderproperty(Config.Circle, "Radius", Value)
+		setrenderproperty(Config.OutlineCircle, "Radius", Value)
     end})
     Section:dropdown({name = "fov position", items = {"center", "mouse"}, flag = Flag .. "_fov_position", callback = function(Value)
-        						if Value == "center" then 
-        						    local size = workspace.CurrentCamera.ViewportSize
-        						    setrenderproperty(Config.Circle, "Position", Vector2.new(size.X / 2, size.Y / 2))
-        						    setrenderproperty(Config.OutlineCircle, "Position", Vector2.new(size.X / 2, size.Y / 2))
-        						end
+        if Value == "center" then 
+        	local size = workspace.CurrentCamera.ViewportSize
+        	setrenderproperty(Config.Circle, "Position", Vector2.new(size.X / 2, size.Y / 2))
+        	setrenderproperty(Config.OutlineCircle, "Position", Vector2.new(size.X / 2, size.Y / 2))
+        end
     end})
 end 
+
+function MONT3R_ENV:BuildLocalPlayer(Section, Flag)
+    Section:toggle({name = "speed changer", flag = Flag .. "_local_speed_enabled"})
+    Section:keybind({listname = "speed", flag = Flag .. "_local_speed_key"})
+    Section:dropdown({name = "speed method", items = {"normal", "cframe"}, flag = Flag .. "_local_speed_method"})
+    Section:slider({name = "speed value", max = 200, flag = Flag .. "_local_speed_value"})
+    Section:toggle({name = "jump power changer", flag = Flag .. "_local_jump_enabled"})
+    Section:keybind({flag = "_local_jump_key"})
+    Section:slider({name = "power value", max = 150, flag = Flag .. "_local_jump_value"})
+    Section:toggle({name = "fly", flag = Flag .. "_local_fly_enabled"})
+    Section:dropdown({name = "fly method", items = {"velocity", "platform", "cframe"}, flag = Flag .. "_local_fly_method"})
+    Section:slider({name = "fly speed", min = 1, max = 100, flag = Flag .. "_local_fly_speed"})
+end
+
+function MONT3R_ENV:BuildBloom(Section, Flag, Bloom)
+    local Bloom = Bloom or Instance.new("BloomEffect", cloneref(game:GetService("Lighting")))
+    Section:toggle({name = "enabled", flag = Flag .. "_bloom_enabled", callback = function(Value)
+        Bloom.Enabled = Value
+    end})
+    Section:slider({name = "intensity", max = 100, flag = "_bloom_intensity", callback = function(Value)
+        Bloom.Intensity = Value / 10
+    end})
+    Section:slider({name = "threshold", max = 1, intervals = 3, flag = Flag .. "_bloom_threshold", callback = function(Value)
+        Bloom.Threshold = Value
+    end})
+    Section:slider({name = "size", max = 100, flag = Flag .. "_bloom_size", callback = function(Value)
+        Bloom.Size = Value
+    end})
+end
+
+function MONT3R_ENV:BuildColor(Section, Flag, Color)
+    local Color = Color or Instance.new("ColorCorrectionEffect", cloneref(game:GetService("Lighting")))
+    Section:toggle({name = "enabled", flag = Flag .. "_color_enabled", callback = function(Value)
+        Color.Enabled = Value
+    end})
+    Section:colorpicker({flag = Flag .. "_color_value", color = Color3.fromRGB(100,100,255), callback = function(Value)
+        Color.TintColor = Value
+    end})
+    Section:slider({name = "brightness", flag = Flag .. "_color_brightness", min = -2.5, max = 2.5, default = 0, intervals = 3, callback = function(Value)
+        Color.Brightness = Value
+    end})
+    Section:slider({name = "contrast", flag = Flag .. "_color_contrast", min = -2.5, max = 2.5, default = 0, intervals = 3, callback = function(Value)
+        Color.Contrast = Value == 0 and 0 or Value * 1.25
+    end})
+    Section:slider({name = "saturation", flag = Flag .. "_color_saturation", min = -2.5, max = 2.5, default = 0, intervals = 3, callback = function(Value)
+        Color.Saturation = Value
+    end})
+end 
+
+function MONT3R_ENV:BuildRender(Tab, Path)
+    local Path = Path or MONT3R_ENV.RENDER or MONT3R_ENV:GETRENDER() or {}
+    local Enemy, Friendly = Tab:section({name = "enemy"}), Tab:Section({name = "friendly", side = "right"})
+
+    local function TextElement(Section, Flag, Type, TextPath)
+        Section:toggle({name = Type, flag = Flag .. "_enabled"})
+    end 
+
+    for i, v in ipairs({"esp_enemy_", "_esp_friendly_"}) do 
+        local Section = string.find(v, "enemy") and Enemy or Friendly 
+        
+    end 
+end
