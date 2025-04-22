38fc2ac78d46a3b96db9d7a959cf7fe103382c9c
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2025-04-22 10:02:19 -0700

    building!!

--- dependencies/library/builder.lua
+++ dependencies/library/builder.lua
@@ -9,5 +9,15 @@ local builder = {}; do
         return cache 
     end 
 
-    function builder.rageSection
+    function builder.aimAssist(section)
+        section:Toggle({name = "Enabled", flag = "legitAimEnabled"}):Keybind({flag = "legitAimKey", mode = "toggle"}})
+        section:Toggle({name = "Require Mouse Movement", flag = "legitAimMovement"})
+        section:Dropdown({name = "Target Priority", content = {"Head", "Torso", "Arms", "Legs"}, multi = true, flag = "legitAimTarget"})
+        section:Toggle({name = "Use Smoothing", flag = "legitAimSmoothing"})
+        section:Dropdown({name = "Curving Type", content = {"linear"}, flag = "legitAimCurving"})
+        section:Slider({name = "X Smoothing Factor", float = 0.25, default = 100, max = 100, suffix = "%", flag = "legitAimSmoothingX"})
+        section:Slider({name = "Y Smoothing Factor", float = 0.25, default = 100, max = 100, suffix = "%", flag = "legitAimSmoothingY"})
+        
+
+    end 
 end 
