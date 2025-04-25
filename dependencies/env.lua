80e09afb6286e3242059fc8866f10b0481be53dd
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2025-04-25 10:03:24 -0700

    work on env

--- dependencies/env.lua
+++ dependencies/env.lua
@@ -1,3 +1,17 @@
-local function retriveDependencies()
+local env = {
+    utilities = {},
+    library = {},
+    assets = {}
+}; do 
+    local function fetch(url)
+        local success, content = pcall(function()
+            return game:HttpGet(url)
+        end)
 
+        return success, content 
+    end
+
+    
 end 
+
+return env 
