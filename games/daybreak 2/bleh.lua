83e23e50981eb6226ee767322689ba1e0e79fb66
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2025-05-02 22:36:45 -0700

    j

--- games/daybreak 2/bleh.lua
+++ games/daybreak 2/bleh.lua
@@ -1,89 +1,60 @@
-local mont3rEnv = isfile("Mont3r/environment.lua") and readfile("Mont3r/environment.lua") or loadstring(game:HttpGet("https://placeholder"))()
-local funcs = mont3rEnv.utilites.functions
-local library = mont3rEnv.utilities.library.source 
-local connections = mont3rEnv.connections 
+local env = MONT3R_ENV
+local utility = env.dependencies.functions
+local library = env.dependencies.library
+ 
+local services = {}; setmetatable(services, {__index = function(self, key) return cloneref and cloneref(game:GetService(key)) or game:GetService(key)})
 
---[[
-    local services = {}; setmetatable(services, {
-        __index = function(self, key) 
-            return cloneref and cloneref(game:GetService(key)) or game:GetService(key)
-        end
-    })
-]]
-
--- // Setting up variables; and caching methods
-local players = mont3rEnv.services.Players 
-local runService = mont3rEnv.services.RunService 
-local tweenService = mont3rEnv.services.TweenService 
-local userInputService = mont3rEnv.services.UserInputService
-local replicatedStorage = mont3rEnv.services.ReplicatedStorage
-local stats = mont3rEnv.services.Stats 
+local players = services.Players 
+local replicatedStorage = services.ReplicatedStorage
 
 local localPlayer = players.LocalPlayer 
+local playerGui = localPlayer.PlayerGui
 
-local findFirstChild = workspace.FindFirstChild 
-local findFirstChildOfClass = workspace.FindFirstChildOfClass
-local getChildren = workspace.getChildren
-
-local remotes = findFirstChild(replicatedStorage, "Remotes")
-
-local function getKiller() humanoid.Health > 0, character, humanoidRootPart, humanoid 
-    for i, v in ipairs(players:GetPlayers()) do 
-        local alive, char = funcs.isAlive(v)
-        if alive then 
-            local weapon = findFirstChild(char, "Weapon") 
-            if weapon then
-                return v, weapon 
-            end  
-        end 
+local remotes = {}; do 
+    for _, v in ipairs(replicatedStorage.Remotes) do 
+        remotes[v.Name] = v
     end 
 
-    return nil, nil 
+    remotes["SetLookAngles"] = replicatedStorage:FindFirstChild("SetLookAngles")
 end 
 
-local function getRemotes(object)
-    local cache = {}
+local function getMatchData()
+    local cache = {} -- Map, GameType, GensLeft, RoundTime 
+
+    for i, v in ipairs(workspace.Bools) do 
+        if v.Name ~= "IntermissionTime" or v.Name ~= "PregameTime" then 
+            table.insert(cache, v)
+        end 
+    end 
 
     return cache 
 end 
 
+local function getKillerData()
+    for i, v in ipairs(players:GetPlayers()) do 
+        local weapon = v.Character:FindFirstChild("Weapon")
+        if weapon then 
+            return v, weapon, weapon:GetAttribute("Killer")
+        end 
+    end 
+
+    return nil, nil, "Slasher"
+end 
+
+local function getMap()
+    local map = workspace.Map_Holder:FindFirstChild("Game_Map")
+
+    if map then 
+        return map 
+    end 
+end 
+
 local function connectCheckHandler(object)
-    
-    local success, message 
-    success, message = pcall(function()
-        funcs.connect(object.ChildAdded, function(child)
-            if string.find(child.Name, localPlayer.Name) then 
-                remotes.skillCheck("Hit", child)
-            end 
-        end, "checkHandler")
-    end
+    assert(object.Name == "HUD", "Wrong object, object name should be HUD!")
 
-    if not success then 
-        warn("connectCheckHandler : failed to write to childAdded, " .. message)
-        return 
-    end 
+    utility.connect(object.ChildAdded, function(child)
+        if string.find(child.Name, localPlayer.Name) then 
 
-    success, message = pcall(function()
-        funcs.connect(object.descendant, function()
-            if object.Parent == nil then
-                if connections["checkHandler"] then 
-                    connections["checkHandler"]:Disconnect()
-                    connections["checkDescendantHandler"]:Disconnect()
-                end 
-            end 
-        end, "checkDescendantHandler") -- signal placeholder; change later
-    end 
-
-    if not success then 
-        warn("connectCheckHandler : failed to write to descendant event, " .. message)
-        return 
-    end 
-    
-    return 
+        end 
+    end, "checkHandler")
 end 
-
-local survivorAnimations, killerAnimations = { names = {}, assets = {} }, { names = {}, assets = {} }; do 
-
-end 
-
-
