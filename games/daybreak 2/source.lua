bfbcdff41f4c345d38a44d2ea8b905ac97583a7c
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2025-05-08 09:35:45 -0700

    n

--- games/daybreak 2/source.lua
+++ games/daybreak 2/source.lua
@@ -3,6 +3,11 @@ local script = {
     environment = MONT3R_ENV or ENV or mont3rEnv or {},
     services = {},
     remotes = {},
+    connections = {},
+    toggles = {},
+    options = {},
+    library = {},
+    emotes = {},
     gameData = {
         map = "",
         gameType = "",
@@ -23,33 +28,20 @@ local script = {
     end 
 })
 
-local players = services.Players 
-local lighting = services.Lighting
-local replicatedStorage = services.ReplicatedStorage
-local workspace = services.Workspace 
+local players = script.services.Players 
+local lighting = script.services.Lighting
+local replicatedStorage = script.services.ReplicatedStorage
+local workspace = script.services.Workspace 
+
+local localPlayer = players.LocalPlayer 
+local camera = workspace.CurrentCamera 
+local playerGui = localPlayer.PlayerGui 
 
 local findFirstChild = workspace.FindFirstChild
 
-function script:getKillerData()
-    for i, v in ipairs(players:GetPlayers()) do 
-        local weapon = findFirstChild(v.Character, "Weapon")
-        if weapon then 
-            local killer = weapon:GetAttribute("Killer")
+local hud = findFirstChild(playerGui, "HUD") 
 
-            self.killerData.player = v 
-            self.killerData.weapon = weapon 
-            self.killerData.name = killer 
-
-            return v, weapon, killer
-        end 
-    end 
-
-    return nil, nil, "Slasher"
-end 
-
-function script:getMap()
-    return findFirstChild(mworkspace.Map_Holder, "Game_Map")
-end 
+local bloomEffect, colorCorrectionEffect = Instance.new("BloomEffect"), Instance.new("ColorCorrectionEffect")
 
 for i, v in ipairs(replicatedStorage.Remotes) do 
     script.remotes[v.Name] = v 
@@ -57,15 +49,87 @@ for i, v in ipairs(replicatedStorage.Remotes) do
     remotes["SetLookAngles"] = findFirstChild(replicatedStorage, "SetLookAngles")
 end 
 
+if require then 
+    script.emotes = require(replicatedStorage.Stickers).GrabAll 
+else
+    script.emotes = loadstring(game:HttpGet("", true))()
+end
+
+do 
+    function script:connect(index, signal, callback)
+        local connection = signal:Connect(callback)
+        self.connections[index] = connection
+        return connection  
+    end 
+    
+    function script:getKillerData()
+        for i, v in ipairs(players:GetPlayers()) do 
+            local weapon = findFirstChild(v.Character, "Weapon")
+            if weapon then 
+                local killer = weapon:GetAttribute("Killer")
+
+                self.killerData.player = v 
+                self.killerData.weapon = weapon 
+                self.killerData.name = killer 
+
+                return v, weapon, killer
+            end 
+        end 
+
+        return nil, nil, "Slasher"
+    end 
+
+    function script:getMap()
+        return findFirstChild(mworkspace.Map_Holder, "Game_Map")
+    end 
+
+    function script:applyHandler(object, extrafunc)
+        local func = extrafunc or function() end 
+        self:connect("skillCheckHandler", object.ChildAdded, function(child)
+            if string.find(child.Name, localPlayer.Name) then 
+                self.remotes.skillCheck:FireServer("Hit", child.Name)
+                func("fire")
+            end 
+        end)
+        self:connect("skillCheckAncestryHandler", object.AncestryChanged, function()
+            if not object:IsDescendantOf(game) then 
+                func("disconnect")
+                self.connections["skillCheckHandler"]:Disconnect()
+                self.connections["skillCheckAncestryHandler"]:Disconnect()
+            end 
+        end)
+        func("created")
+    end
+end
+
+script:connect("hudHandler", playerGui.ChildAdded, function(child)
+    if child.Name == "HUD" then 
+        hud = child
+        script:applyHandler(child)
+    end 
+end)
+
+if hud then 
+    script:applyHandler(hud)
+end 
+
 local oldNameCall; oldNameCall = hookmetamethod(game, "__namecall", function(self, ...) -- use newcclosure later
     local method, args = getnamecallmethod(), {...}
 
-    if method == "FireServer" and self.Name == "SetLookAngles" then 
-        if placeHolder
-        args = {
-            placeHolder1,
-            placeHolder2
-        }
+    if method == "FireServer" then 
+    --[[
+        if self.Name == "SetLookAngles" and PLACEHOLDER then 
+            args = {
+                (-1 - 1),
+                (-1 - 1)
+            }
+        end
+        
+        if self.Name == "skillCheck" and PLACEHOLDER then
+            args[1] = "Hit"
+        end
+    ]]
+        
     end 
 
     return oldNameCall 
