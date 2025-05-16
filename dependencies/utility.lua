5ba75049d9aa32c57e093535e0c88e2c7556f088
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2025-05-16 14:12:13 -0700

    s

--- dependencies/utility.lua
+++ dependencies/utility.lua
@@ -1,22 +1,42 @@
+local cloneref = cloneref or function(object : any) return object end 
+local identifyExecutor = identifyexecutor or isexecutor or function() return "" end
+local drawing = Drawing or {new = function(...) return {} end}
+local setRenderProperty = setrenderproperty or function(obj, idx, val) obj[idx] = val end 
+
+local services = {}; setmetatable(services, { __index = function(self, key) return cloneref(game:GetService(key) end })
 local library = {
     connections = {},
     instances = {},
     drawings = {},
 
-    directory = "mont3r"
-}; setmetatable(library, {
-    __index = function(self, key)
-        return game:GetService(key)
-    end 
-})
-
-local cloneref = cloneref or function(object : any) return object end 
-local identifyexecutor = identifyexecutor or isexecutor or function() return "" end
-
-function library.log(content)
-    warn("utility library - " .. content)
-end 
+    directory = "mont3r",
+    debug = false
+};
 
 function library.connect(signal, func)
-    local connection = signal
+    local connection = signal:Connect(func)
+    --
+    library.connections[#library.connections + 1] = connection 
+    --
+    return connection 
+end 
+
+function library.create(instance, properties)
+    local object = Instance.new(instance)
+    --
+    for i, v in ipairs(properties) do 
+        object[i] = v 
+    end 
+    --
+    return object 
+end 
+
+function library.draw(drawing, properties)
+    local object = drawing.new(drawing)
+    --
+    for i, v in ipairs(properties) do 
+        setRenderProperty(object, i, v)
+    end 
+    --
+    return object
 end 
